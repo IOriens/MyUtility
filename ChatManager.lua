eeeb779706c76f431e67bf50efbcedb3e35e3717
dingjunjie02 <dingjunjie02@meituan.com>
2024-10-10 09:00:12 +0800

    1

--- ChatManager.lua
+++ ChatManager.lua
@@ -98,8 +98,8 @@ local fiveStarString = "法杖全等级带美化免费稳5，接再造，也是�
 local autoReplies = {
   { keyword = "火炬", reply = "副手不会做哈～" },
   { keyword = "副手", reply = "副手不会做哈～" },
-  { keyword = "面杖", reply = "专业工具不接哈，不能稳5～" },
-  { keyword = "羽毛", reply = "专业工具不接哈，不能稳5～" },
+  { keyword = "面杖", reply = "不接哈，不能稳5～" },
+  { keyword = "羽毛", reply = "不接哈，不能稳5～" },
   { keyword = "饰品", reply = "不会做哈～" },
   { keyword = "布甲", reply = "不会做哈～" },
   { keyword = "徽记", reply = "不会做哈～" },
@@ -752,7 +752,7 @@ local function Initialize()
   C_Timer.After(3, function()
     --
     -- 默认显示窗口
-    frame:Show()
+    -- frame:Show()
   end)
 end
 

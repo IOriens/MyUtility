733f692ecc250db585251d11510f041839ce3dc8
Junjie Ding <djjoriens@gmail.com>
2024-10-31 23:22:00 +0800

    1

---
+++ SendMail.lua
@@ -0,0 +1,14 @@
+function SendMailWithMoney()
+  if not MailFrame:IsVisible() then
+    print("请打开邮箱")
+    return
+  end
+
+  MailFrameTab_OnClick(nil, 2)
+  SendMailSubjectEditBox:SetText("1");
+  SendMailNameEditBox:SetText("夜间漫游");
+  local money = GetMoney() / 10000 - 10000;
+  -- 取整
+  money = math.floor(money);
+  SendMailMoneyGold:SetText(tostring(money));
+end

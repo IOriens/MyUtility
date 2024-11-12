7c1991cb1842cb77bf55563e55c5e0a5f8802208
Junjie Ding <djjoriens@gmail.com>
2024-11-12 22:58:50 +0800

    1

--- AuctionThirdAttribute.lua
+++ AuctionThirdAttribute.lua
@@ -1,36 +1,36 @@
 
-local function ShowThirdAttribute(row, itemID)
-  if not itemID then return end
+local function ShowThirdAttribute(row, itemLink)
 
-  -- 异步获取物品信息
-  local item = Item:CreateFromItemID(itemID)
-  item:ContinueOnItemLoad(function()
-      local itemLink = item:GetItemLink()
-      if not itemLink then return end
+  -- 获取物品的属性
+  local stats =  C_Item.GetItemStats(itemLink)
+  if not stats then return end
 
-      -- 获取物品的属性
-      local stats =  C_Item.GetItemStats(itemLink)
-      if not stats then return end
+  local attributes = {}
 
-      local attributes = {}
+  -- 指定要显示的属性
+  local desiredStats = {
+    ["ITEM_MOD_CR_SPEED_SHORT"] = true,     -- 加速
+    -- ["ITEM_MOD_CR_AVOIDANCE_SHORT"] = true,  -- 闪避
+    -- ["ITEM_MOD_CR_LIFESTEAL_SHORT"] = true,  -- 吸血
+  }
 
-      -- 遍历获取第三属性
-      for stat, value in pairs(stats) do
-          if value > 0 then
-              print(stat, value)
-              table.insert(attributes, _G[stat] .. ": " .. value)
-          end
-      end
+  DevTools_Dump(stats)
 
-      -- 显示第三属性（仅显示前三个属性）
-      if #attributes > 0 then
-          local thirdAttrText = table.concat(attributes, ", ", 1, 3)
-          row.ThirdAttribute:SetText(thirdAttrText)
-          row.ThirdAttribute:Show()
-      else
-          row.ThirdAttribute:Hide()
-      end
-  end)
+  -- 遍历获取指定的属性
+  for stat, value in pairs(stats) do
+    if value > 0 and desiredStats[stat] then
+      table.insert(attributes, _G[stat] .. ": " .. value)
+    end
+  end
+
+  -- 显示属性（仅显示前三个）
+  if #attributes > 0 then
+    local thirdAttrText = table.concat(attributes, ", ")
+    row.ThirdAttribute:SetText(thirdAttrText)
+    row.ThirdAttribute:Show()
+  else
+    row.ThirdAttribute:Hide()
+  end
 end
 
 local function OnEvent(self, event)
@@ -39,17 +39,17 @@ local function OnEvent(self, event)
     hooksecurefunc(AuctionHouseFrame.ItemBuyFrame.ItemList.ScrollBox, "Update", function(self)
       for _, row in ipairs(self:GetFrames()) do
         if row.rowData then
-          local itemID = row.rowData.itemKey.itemID
+          local itemLink = row.rowData.itemLink
           -- 如果第三属性标签不存在，则创建
           if not row.ThirdAttribute then
             row.ThirdAttribute = row:CreateFontString(nil, "OVERLAY", "GameFontNormalSmall")
-            row.ThirdAttribute:SetPoint("BOTTOMLEFT", row, "BOTTOMLEFT", 200, 4)
+            row.ThirdAttribute:SetPoint("BOTTOMLEFT", row, "BOTTOMLEFT", 10, 4)
             row.ThirdAttribute:SetTextColor(0.5, 0.8, 0.5)
           end
 
 
           -- 显示第三属性
-          ShowThirdAttribute(row, itemID)
+          ShowThirdAttribute(row, itemLink)
         end
       end
     end)
